2f4a6c93b1894727ff720e1b41fb5d5bf5d5b07b
Alex Hewson <69345308+alexhewson@users.noreply.github.com>
2022-08-25 12:39:20 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of sentencing outcomes by police force area in England and Wales 2022',
+    author='Alex Hewson',
+    license='MIT',
+)
