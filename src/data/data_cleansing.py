09fdafaeb9530f255bc42e661b479f15ec10cbb8
Alex Hewson <69345308+alexhewson@users.noreply.github.com>
2023-04-20 11:52:26 +0100

    Resolving local conflict

--- src/data/data_cleansing.py
+++ src/data/data_cleansing.py
@@ -7,11 +7,11 @@ import pandas as pd
 ## IMPORTING DATASETS ##
 # 1. Sentencing data 2017–21 (from: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1076592/Data-behind-interactive-tools-3.zip)
 cols = ['Police Force Area', 'Year', 'Sex', 'Age group', 'Offence group', 'Sentence Outcome', 'Custodial Sentence Length','Sentenced']
-df = load.loadData("data/external/sentencing.csv", cols=cols)
+df = load.loadData("../../../data/external/sentencing.csv", cols=cols)
 
 # 2. Court outcomes by police force area 2009–2019 (from: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/888561/csvs-behind-data-tools-2-2019.zip)
 cols_2009 =['Police Force Area', 'Year of Appearance', 'Sex', 'Age Group', 'Offence Group', 'Outcome', 'Custodial Sentence Length','Count'] 
-df_2009 = load.loadData('data/external/court-outcomes-by-PFA-2019.csv', cols=cols_2009)
+df_2009 = load.loadData('../../../womens-pfa-analysis/data/external/court-outcomes-by-PFA-2019.csv', cols=cols_2009)
 
 ## DATA CLEANSING FUNCTIONS ##
 
@@ -40,20 +40,33 @@ def renameColumns(data, columns):
         inplace = True
     )
 
-#Setting columns to lowercase and renaming
+def orderColumns(data, column_order):
+    '''
+    Set column order for dataframe
+
+    Parameters
+    ----------
+    data
+
+    column_order: list, ordered in ascending order to be displayed
+    '''
+    data = data[column_order]
+
+#Renaming and re-ordering columns
 
 for data in [df, df_2009]:
     lcColumns(data)
     renameColumns(data, columns={
         'year of appearance': 'year',
         'offence group': 'offence',
-        'age group': 'age',
+        'age group': 'age_group',
         'police force area': 'pfa',
         'sentence outcome': 'outcome',
         'custodial sentence length': 'sentence_len',
         'sentenced': 'freq',
         'count': 'freq'}
         )
+    orderColumns(data, column_order = ['year', 'pfa', 'sex', 'age_group', 'offence', 'outcome', 'sentence_len', 'freq'])
 
 
 
