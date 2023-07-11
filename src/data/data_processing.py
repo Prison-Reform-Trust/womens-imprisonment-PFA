34a68ecdf6884c37e5bd899539353abd60a445a7
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2023-07-11 15:09:46 +0100

    working pipeline for CJS statistics processing

--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -9,40 +9,50 @@ import utilities as utils
 import pandas as pd
 
 #Importing cleansed dataset
-df = pd.read_csv('data/interim/PFA_2009-21_women_cust_comm_sus.csv')
+df = pd.read_csv('data/interim/PFA_2010-22_women_cust_comm_sus.csv')
+
 
 ## 1.SENTENCING OUTCOME FOR EACH PFA BY YEAR
 
-#Grouping dataset
-pfa_sentencing_outcomes = df.groupby(['pfa', 'year', 'outcome'], as_index=False)['freq'].sum()
+#Grouping dataset and saving out
+(
+    df
+    .groupby(['pfa', 'year', 'outcome'], as_index=False)['freq'].sum()
+    .to_csv('data/processed/PFA_2010-22_women_sentencing_outcomes_FINAL.csv', index=False)
+)
+
+
+## 2. CUSTODIAL SENTENCES FOR EACH PFA BY OFFENCE TYPE
+
+#Filtering cleansed dataset and grouping by PFA and offence group 
+sentence_type = 'Immediate custody'
+year = 2022
+
+pfa_custody = (
+    df
+    .query("outcome == @sentence_type & year == @year")
+    .groupby(['pfa', 'offence'], as_index=False)['freq'].sum()
+)
+
+#Using crosstab with normalize argument to calculate offence group proportions by PFA
+pfa_custody_offences = pd.crosstab(index=pfa_custody['pfa'], columns=pfa_custody['offence'], values=pfa_custody['freq'], aggfunc=sum, normalize='index').round(3)
 
 #Outputting to CSV
-pfa_sentencing_outcomes.to_csv('data/processed/PFA_2009-21_women_sentencing_outcomes_FINAL.csv', index=False)
-
-## 2. CUSTODIAL SENTENCES BY OFFENCE TYPE FOR EACH PFA
-
-#Filtering cleansed dataset
-filt1 = df['outcome'] == 'Immediate custody'
-filt2 = df['year'] == 2021
-pfa_custody = df[filt1 & filt2].copy()
-
-"""CURRENTLY HERE, USING CODE FROM PFA_2021_OFFENCES.PY, EXPLORING METHOD CHAINING OPTIONS AND PIPE METHOD"""
+pfa_custody_offences.to_csv('data/processed/PFA_2022_offences.csv')
 
 
-
-
-## 2.CUSTODIAL SENTENCE LENGTHS FOR EACH PFA BY YEAR
+## 3.CUSTODIAL SENTENCE LENGTHS FOR EACH PFA BY YEAR
 '''THIS PRODUCES THE DATA FOR FIGURE 1 IN THE PFA FACTSHEET'''
 
-#Defining sentence_len categories
-less_6months = ["Up to 1 month", 
-                "More than 1 month and up to 2 months",
-                "More than 2 months and up to 3 months",
-                "More than 3 months and under 6 months"]
+#Defining sentence_length categories—THESE HAVE BEEN UPDATED
+less_6months = ["Up to and including 1 month", 
+                "More than 1 month and up to and including 2 months",
+                "More than 2 months and up to and including 3 months",
+                "More than 3 months and up to 6 months"]
 
 six_12_months = ["6 months",
-                "More than 6 months and up to 9 months",
-                "More than 9 months and under 12 months"]
+                "More than 6 months and up to and including 9 months",
+                "More than 9 months and up to 12 months"]
 
 #Mapping sentence_len categories
 def sentence_length_groups(sentence_len):
@@ -54,18 +64,18 @@ def sentence_length_groups(sentence_len):
         return 'Over 12 months'
 
 #Filtering for custodial sentences and applying the map
-filt = df['outcome'] == 'Immediate custody'
-pfa_custody_sentence_lengths = df[filt].copy()
-pfa_custody_sentence_lengths['sentence_len'] = pfa_custody_sentence_lengths['sentence_len'].map(sentence_length_groups)
+
+pfa_custody_sentence_lengths = df.query('outcome == @sentence_type').copy()
+pfa_custody_sentence_lengths['sentence_length'] = pfa_custody_sentence_lengths['sentence_length'].map(sentence_length_groups)
 
 #Grouping dataset
-pfa_custody_sentence_lengths = pfa_custody_sentence_lengths.groupby(['pfa', 'year', 'sentence_len'], as_index=False)['freq'].sum()
+pfa_custody_sentence_lengths = pfa_custody_sentence_lengths.groupby(['pfa', 'year', 'sentence_length'], as_index=False)['freq'].sum()
 
 #Outputting to CSV
-pfa_custody_sentence_lengths.to_csv('data/interim/PFA_2009-21_women_cust_sentence_len.csv', index=False)
+pfa_custody_sentence_lengths.to_csv('data/interim/PFA_2010-22_women_cust_sentence_length.csv', index=False)
 
 
-## 3. CUSTODIAL SENTENCES FOR EACH PFA BY YEAR
+## 4. CUSTODIAL SENTENCES FOR EACH PFA BY YEAR
 
 '''THIS PRODUCES THREE DATASETS: 
     * TOTAL NUMBER OF WOMEN SENTENCED TO CUSTODY BY PFA; AND OF THOSE 
@@ -79,11 +89,11 @@ filt = pfa_custody_sentence_lengths['year'] >= 2014
 pfa_df_2014 = pfa_custody_sentence_lengths[filt].copy()
 
 #By sentences of less than six months
-filt = pfa_df_2014['sentence_len'] == "Less than 6 months"
+filt = pfa_df_2014['sentence_length'] == "Less than 6 months"
 lt_6m = pfa_df_2014[filt].copy()
 
 #By sentences of less than 12 months
-filt = pfa_df_2014['sentence_len'] != "Over 12 months"
+filt = pfa_df_2014['sentence_length'] != "Over 12 months"
 lt_12m = pfa_df_2014[filt].copy()
 
 #Defining new function for aggregating data and adding a percentage change column
@@ -92,7 +102,7 @@ def aggregate_sentences(df):
                         values=df['freq'], aggfunc='sum')
     
     new_df = new_df.fillna(0.0).astype(int)
-    new_df['per_change_2014'] = new_df.pct_change(axis='columns', periods=7).dropna(axis='columns')
+    new_df['per_change_2014'] = new_df.pct_change(axis='columns', periods=8).dropna(axis='columns')
     return new_df
 
 #Using dictionary comprehension to run both DataFrames through the function
