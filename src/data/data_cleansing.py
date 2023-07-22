67be6a0bbf7a585af4e3670568df32abeeeabd14
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2023-07-22 17:54:16 +0100

    2009 dataset removed

--- src/data/data_cleansing.py
+++ src/data/data_cleansing.py
@@ -10,6 +10,7 @@ path="data/external/obo_sent_pivot_2010_2022/"
 all_files = glob.glob(path + "*.csv")
 
 ## IMPORTING DATASETS ##
+
 # 1. Sentencing data 2010–22 
 # (from: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157979/obo_sent_pivot_2010_2015.zip and 
 # https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157991/obo_sent_pivot_2016_2022.zip)
@@ -18,58 +19,43 @@ cols = ['Police Force Area', 'Year', 'Sex', 'Age group', 'Offence group', 'Sente
 all_csvs = [utils.loadData(filename, cols=cols) for filename in all_files]
 df = pd.concat(all_csvs, axis=0, ignore_index=True)
 
-# 2. Court outcomes by police force area 2009–2019 (from: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/888561/csvs-behind-data-tools-2-2019.zip)
-cols_2009 =['Police Force Area', 'Year of Appearance', 'Sex', 'Age Group', 'Offence Group', 'Outcome', 'Custodial Sentence Length','Count'] 
-df_2009 = utils.loadData('../womens-pfa-analysis/data/external/court-outcomes-by-PFA-2019.csv', cols=cols_2009)
-
-#Filtering `df_2009` for 2009 data
-filt = df_2009['Year of Appearance'] == 2009
-df_2009 = df_2009[filt].copy()
-
-# Defining datasets to iterate through in following section
-df_list = utils.dataframeList(locals()) #Using locals() function to retrieve local symbol table. Note this outputs a complex list, and is no longer a DataFrame.
-
 ## DATA CLEANING PROCESS
 
 # Renaming columns
-for data in df_list:
-    utils.lcColumns(data)
-    utils.renameColumns(data, columns={
-        'year_of_appearance': 'year',
-        'offence_group': 'offence',
-        'police_force_area': 'pfa',
-        'sentence_outcome': 'outcome',
-        'custodial_sentence_length': 'sentence_length',
-        'sentenced': 'freq',
-        'count': 'freq'}
-        )
+utils.lcColumns(df)
+utils.renameColumns(df, columns={
+    'year_of_appearance': 'year',
+    'offence_group': 'offence',
+    'police_force_area': 'pfa',
+    'sentence_outcome': 'outcome',
+    'custodial_sentence_length': 'sentence_length',
+    'sentenced': 'freq',
+    'count': 'freq'}
+    )
 
-# Joining and tidying elements into one DataFrame
-df_combined = pd.concat(df_list)
-utils.tidy_elements(df_combined)
+# Tidying elements using regex function
+utils.tidy_elements(df)
 
 # Reordering columns
 column_order = ['year', 'pfa', 'sex', 'age_group', 'offence', 'outcome', 'sentence_length', 'freq']
-df_combined = df_combined.reindex(columns=column_order)
+df = df.reindex(columns=column_order)
 
 # Setting categorical columns
 convert_dict = {'outcome': "category",
                 'sentence_length': "category"
                 }
-df_combined = df_combined.astype(convert_dict)
+df = df.astype(convert_dict)
 
-# Setting `outcomes` and `sentence_length` to capitalize case
-capitalize_columns = ['outcome', 'sentence_length']
-for column in capitalize_columns:
-    df_combined[column] = df_combined[column].str.capitalize()
+# Setting outcomes to lowercase
+df['outcome'] = df['outcome'].str.capitalize()
 
 ## FILTERING DATASET
-filt1 = df_combined['sex'] == 'Female'
-filt2 = df_combined['outcome'].isin(['Immediate custody', 'Community sentence','Suspended sentence'])
-filt3 = df_combined['age_group'].isin(["Adults", "Young adults"])
-filt4 = df_combined['pfa'].isin(["Special/miscellaneous and unknown police forces", "City of London", "Not known"])
+filt1 = df['sex'] == 'Female'
+filt2 = df['outcome'].isin(['Immediate custody', 'Community sentence','Suspended sentence'])
+filt3 = df['age_group'].isin(["Adults", "Young adults"])
+filt4 = df['pfa'].isin(["City of London", "Not known"])
 filt = filt1 & filt2 & filt3 & ~filt4
-women_dataset = df_combined[filt].sort_values(['year', 'pfa']).copy()
+women_dataset = df[filt].sort_values(['year', 'pfa']).copy()
 
 ## OUTPUTTING INTERIM DATASET FOR FURTHER ANALYSIS
-women_dataset.to_csv('data/interim/PFA_2009-22_women_cust_comm_sus.csv', index=False)
+women_dataset.to_csv('data/interim/PFA_2010-22_women_cust_comm_sus.csv', index=False)
