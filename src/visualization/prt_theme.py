8fc1044c9a439bd44a80f3560fb2e3cb74b0aeec
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2023-08-22 15:16:34 +0100

    Added plotly theme with PRT style parameters

---
+++ src/visualization/prt_theme.py
@@ -0,0 +1,38 @@
+#Custom plotly template for all charts in this project
+
+import plotly.io as pio
+import plotly.graph_objs as go
+
+#PRT standard template
+pio.templates["prt_template"] = go.layout.Template(
+    layout=go.Layout(
+        title_font=dict(family="Helvetica Neue, Arial", size=17),
+        title_y=0.94,
+        title_yanchor="bottom",
+        font_color="#54565B",
+        font_family="Helvetica Neue, Arial",
+        font_size=12,
+        paper_bgcolor="#F7F2F2",
+        plot_bgcolor="#F7F2F2",
+        colorway=("#A01D28", "#499CC9", "#F9A237", "#6FBA3A", "#573D6B"),
+        modebar_activecolor="#A01D28",
+        showlegend=False,
+        xaxis_showgrid=False,
+        xaxis_tickcolor="#54565B",
+
+    )
+)
+## Chart annotations
+def sourceAnnotation(source, annotations_list):
+    annotations_list.append(
+        dict(
+            xref="paper",
+            yref="paper",
+            x=-0.08,
+            y=-0.19,
+            align="left",
+            showarrow=False,
+            text=f"Source: {source}",
+            font_size=12,
+        )
+    )
