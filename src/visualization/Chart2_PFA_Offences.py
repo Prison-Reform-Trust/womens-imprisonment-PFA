f8381d18b1362cc04054db611d806b5775aecf7c
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2024-03-12 11:45:04 +0000

    Refactoring Chart2 with PEP 8 and other minor adjustments

--- src/visualization/Chart2_PFA_Offences.py
+++ src/visualization/Chart2_PFA_Offences.py
@@ -1,46 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# IMPORTING LIBRARIES
 import pandas as pd
 import textwrap
 import plotly.graph_objs as go
 import plotly.io as pio
-from pathlib import Path #To create unique filenames for each PFA chart
+from pathlib import Path
 
-# IMPORTING LOCAL SCRIPTS/CONFIG/TEMPLATE
-import src.data.utilities as utils
-import src.visualization.prt_theme
+from src.data import utilities as utils
+from src.visualization import prt_theme
+
 config = utils.read_config()
 pio.templates.default = "prt_template"
 
-##FUNCTIONS AND CLASSES
 
-class pfaOffencesChart:
-    
-    def __init__(self, pfa:str, df:pd.DataFrame):
-        
+class PfaOffencesChart:
+
+    def __init__(self, pfa: str, df: pd.DataFrame):
         self.pfa = pfa
         self.df = df
-        self.trace_list = [] # Need to empty my trace_list with every loop through each PFA so that charts are plotted separately
-        self.annotations: list[dict] = []  # Add this line to provide a type hint for self.annotations
-        self.fig = go.Figure() # Need to also instantiate the figure with every loop in order to clear fig.data values
+        self.trace_list = []
+        self.annotations: list[dict] = []
+        self.fig = go.Figure()
 
-    def customWrap(self, s:str, width=19):
+    @staticmethod
+    def custom_wrap(s: str, width=19):
         return "<br>".join(textwrap.wrap(s, width=width))
-    
-    def prepareData(self):
+
+    def prepare_data(self):
         #Melting df from wide to long
-        id_vars=['pfa']
-        value_vars = list(self.df.columns[1:])  # Convert Index to list
+        id_vars = ['pfa']
+        value_vars = list(self.df.columns[1:])
         self.df = pd.melt(self.df, id_vars=id_vars, value_vars=value_vars, var_name='offence', value_name='proportion')
 
-        #Selecting the offences that I want to continue to display at the root of the sunburst diagram
         highlighted_offence_groups = ['Theft offences', 'Drug offences', 'Violence against the person']
         self.filt = self.df['offence'].isin(highlighted_offence_groups)
 
-        self.df['parent'] = "" # Creating a new object column to prevent FutureWarning being triggered
-        self.df.loc[self.filt, 'parent'] = "All offences" # This method prevents that annoying copy/view warning
+        self.df['parent'] = ""
+        self.df.loc[self.filt, 'parent'] = "All offences"
         self.df.loc[~self.filt, 'parent'] = "All other<br>offences"
 
         #Setting discreet plotting order
@@ -51,27 +48,27 @@ class pfaOffencesChart:
             'Violence against the person': 3
         }
         self.df['plot_order'] = self.df["offence"].map(plot_dict).fillna(0)
-        
+
         #Wrapping longer offence text
-        self.df.loc[self.filt, 'offence'] = self.df.loc[self.filt, 'offence'].map((lambda x: self.customWrap(x, width=12))) # Wrapping highlighted_offence_groups with a smaller width argument
-        self.df.loc[~self.filt, 'offence'] = self.df.loc[~self.filt, 'offence'].map(self.customWrap) # Wrapping all other offences
+        self.df.loc[self.filt, 'offence'] = self.df.loc[self.filt, 'offence'].map(
+            (lambda x: self.custom_wrap(x, width=12)))
+        self.df.loc[~self.filt, 'offence'] = self.df.loc[~self.filt, 'offence'].map(self.custom_wrap)
 
         return self.df
 
-    def createTraces(self):
+    def create_traces(self):
         self.df = self.df[self.df["pfa"] == self.pfa]
         self.df = pd.concat([
             self.df,
             pd.DataFrame.from_records([{
-                'pfa': self.df['pfa'].iloc[0], 
-                'offence':"All other<br>offences", 
-                'proportion': self.df.loc[~self.filt, 'proportion'].sum(), 
-                'parent':"All offences",
+                'pfa': self.df['pfa'].iloc[0],
+                'offence': "All other<br>offences",
+                'proportion': self.df.loc[~self.filt, 'proportion'].sum(),
+                'parent': "All offences",
                 'plot_order': 0
             }])
         ], ignore_index=True).sort_values(by=['plot_order', 'proportion'], ascending=True)
 
-        # Creating the Sunburst trace
         sunburst_trace = go.Sunburst(
             labels=self.df['offence'],
             parents=self.df['parent'],
@@ -89,21 +86,20 @@ class pfaOffencesChart:
 
         return self.fig.add_trace(sunburst_trace)
 
-    def chartParams(self):
-        ## Chart title
-        title = textwrap.wrap(f'<b>Imprisonment of women in {self.df["pfa"].iloc[0]} by offence group in 2022</b>', width=45)
+    def chart_params(self):
+        title = textwrap.wrap(f'<b>Imprisonment of women in {self.df["pfa"].iloc[0]} by offence group in 2022</b>',
+                              width=45)
 
         self.fig.update_layout(
-            margin = dict(t=75, l=0, r=0, b=0),
+            margin=dict(t=75, l=0, r=0, b=0),
             title="<br>".join(title),
             title_y=0.94,
             title_yanchor="bottom",
             width=630,
             height=630,
-            )
-    
-    def chartAnnotations(self):
-        
+        )
+
+    def chart_annotations(self):
         # Adding source label
         self.annotations.append(
             dict(
@@ -122,43 +118,42 @@ class pfaOffencesChart:
         # Adding annotations to layout
         return self.fig.update_layout(annotations=self.annotations)
 
-
-    def saveChart(self, folder: str, filetype: str ='pdf'):
+    def save_chart(self, folder: str, filetype: str = 'pdf'):
         self.filetype = filetype
         self.folder = folder
 
-        if self.trace_list == []: #Allows saveChart method to run without outputChart requirement
-            self.prepareData()
-            self.createTraces()
-            self.chartParams()
-            self.chartAnnotations()
+        if not self.trace_list:
+            self.prepare_data()
+            self.create_traces()
+            self.chart_params()
+            self.chart_annotations()
 
         export_path = Path.joinpath(Path.cwd(), f"{config['data']['outPath']}", f"{self.folder}/{self.filetype}")
-        export_path.mkdir(parents=True, exist_ok=True) #generate if does not exist
+        export_path.mkdir(parents=True, exist_ok=True)
 
-        # Setting filename variable and full path
         filename = str(self.df["pfa"].iloc[0])
         export_path = Path.joinpath(export_path, f'{filename}.{self.filetype}')
-    
-        self.fig.write_image(export_path)
-    
 
-    def outputChart(self):
-        self.prepareData()
-        self.createTraces()
-        self.chartParams()
-        self.chartAnnotations()
+        self.fig.write_image(export_path)
+
+    def output_chart(self):
+        self.prepare_data()
+        self.create_traces()
+        self.chart_params()
+        self.chart_annotations()
         self.fig.show()
 
-def makePfaOffencesCharts(filename:str, folder:str, status: str ='processed', filetype: str ='pdf'):
-    df = utils.loadData(status, filename)
+
+def make_pfa_offences_charts(filename: str, folder: str, status: str = 'processed', filetype: str = 'pdf'):
+    df = utils.load_data(status, filename)
     df = df.rename({'Fraud Offences': 'Fraud offences'}, axis=1)
     for pfa in df['pfa'].unique():
-        chart = pfaOffencesChart(pfa, df)
-        chart.saveChart(folder, filetype)
+        chart = PfaOffencesChart(pfa, df)
+        chart.save_chart(folder, filetype)
     print("Charts ready")
 
+
 if __name__ == "__main__":
-    filename='PFA_2022_offences.csv'
-    folder='custody_offences_2022'
-    makePfaOffencesCharts(filename, folder)
+    filename = 'PFA_2022_offences.csv'
+    folder = 'custody_offences_2022'
+    make_pfa_offences_charts(filename, folder)
