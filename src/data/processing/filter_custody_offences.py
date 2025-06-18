3cfed7896343590c34afd457e407258348cbda77
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2025-06-18 16:58:52 +0100

    Moved data processing functions from visualisation script custody_offences.py to data processing script filter_custody_offences.py: includes melt_dataframe, filter_offences, set_parent_column, set_plot_order, and wrap_offence_text

--- src/data/processing/filter_custody_offences.py
+++ src/data/processing/filter_custody_offences.py
@@ -12,6 +12,7 @@ This script is part of the data processing pipeline, and results the following d
 """
 
 import logging
+import textwrap
 
 import pandas as pd
 
@@ -66,15 +67,113 @@ def calculate_offence_proportions(df: pd.DataFrame) -> pd.DataFrame:
         normalize='index').round(3)
 
 
+def melt_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Melt the DataFrame from wide to long format.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame to melt.
+
+    Returns
+    -------
+    pd.DataFrame
+        The melted DataFrame.
+    """
+    logging.info("Melting DataFrame to long format...")
+    return pd.melt(
+        df,
+        id_vars=['pfa'],
+        value_vars=list(df.columns[1:]),
+        var_name='offence',
+        value_name='proportion')
+
+
+def filter_offences(df: pd.DataFrame) -> bool:
+    """
+    Creates a boolean mask to filter processing on DataFrame rows that
+    are in the specified offence groups.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame containing offence data.
+
+    Returns
+    -------
+    bool
+        A boolean mask indicating whether each row's offence is in the specified groups.
+    """
+    logging.info("Creating filter for highlighted offence groups...")
+    highlighted_offence_groups = ['Theft offences', 'Drug offences', 'Violence against the person']
+    return df['offence'].isin(highlighted_offence_groups)
+
+
+def set_parent_column(df: pd.DataFrame, filter_mask: pd.Series) -> pd.DataFrame:
+    """
+    Set the parent column in the DataFrame based on the filter mask.
+
+    Note: This function modifies the input DataFrame in place.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame to modify.
+    filter_mask : pd.Series
+        A boolean Series indicating which rows are highlighted.
+
+    Returns
+    -------
+    pd.DataFrame
+        The modified DataFrame with the parent column set.
+    """
+    logging.info("Setting parent column for offence groups...")
+    df.loc[filter_mask, 'parent'] = "All offences"
+    df.loc[~filter_mask, 'parent'] = "All other\noffences"
+    return df
+
+
+def set_plot_order(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Assigns a plotting order to offences in the DataFrame based on predefined categories.
+
+    Parameters:
+        df (pd.DataFrame): Input DataFrame containing an 'offence' column.
+
+    Returns:
+        pd.DataFrame: DataFrame with an added 'plot_order' column, where each offence is mapped to an integer
+                      representing its plotting order. Offences not in the predefined list are assigned 0.
+    """
+    logging.info("Setting plot order for offences...")
+    plot_dict = {
+        'All other offences': 0,
+        'Theft offences': 1,
+        'Drug offences': 2,
+        'Violence against the person': 3
+    }
+    df['plot_order'] = df["offence"].map(plot_dict).fillna(0)
+    return df
+
+
+def wrap_offence_text(df: pd.DataFrame, filter_mask: pd.Series) -> pd.DataFrame:
+    df['offence'] = df.apply(
+        lambda row: textwrap.fill(row['offence'], width=12 if filter_mask.loc[row.name] else 19),
+        axis=1
+    )
+    return df
+
+
 def load_and_process_data() -> tuple[pd.DataFrame, int]:
     """
     Load the interim dataset and process it to filter custodial sentences,
-    latest year and group by PFA.
+    select the latest year, group by PFA and offence, calculate proportions,
+    and return a melted DataFrame ready for plotting.
 
     Returns
     -------
     tuple[pd.DataFrame, int]
-        The processed DataFrame and the latest year used in filtering.
+        The processed and melted DataFrame ready for plotting, and the latest year used in filtering.
     """
     df = (
         utils.load_data(status='interim', filename=INPUT_FILENAME)
@@ -87,8 +186,17 @@ def load_and_process_data() -> tuple[pd.DataFrame, int]:
         .pipe(filter_years.get_year, year_from=max_year)
         .pipe(group_by_pfa_and_offence)
         .pipe(calculate_offence_proportions)
+        .reset_index()
+        .pipe(melt_dataframe)
     )
 
+    filter_mask = filter_offences(df)
+    df = (
+        set_parent_column(df, filter_mask)
+        .pipe(set_plot_order)
+        .pipe(wrap_offence_text, filter_mask)
+        )
+    logging.info("Data successfully processed for PFA offences charts")
     return df, max_year
 
 
@@ -104,8 +212,7 @@ def main():
     utils.safe_save_data(
         df,
         path=config['data']['clnFilePath'],
-        filename=filename,
-        index=True
+        filename=filename
     )
 
 
